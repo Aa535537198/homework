530e3d7a9a1adbdee77df18489d6fc3bbc1013ad
jgod <535537198@qq.com>
2020-12-06 16:55:20 +0800

    study

--- src/main/java/day_1/SelectSort.java
+++ src/main/java/day_1/SelectSort.java
@@ -20,37 +20,10 @@ public class SelectSort {
     }
 
     private static int[] select(int[] arr) {
-        int minIndex;
-
-        for (int i = 0; i < arr.length - 1; i++) {
-            minIndex = i;
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[minIndex]){
-                    minIndex = j;
-                }
-            }
-
-                int temp = arr[minIndex];
-                arr[minIndex] = arr[i];
-                arr[i] = temp;
-        }
-        return arr;
+        return null;
     }
 
     private static int[] selectMax(int[] arr){
-        int maxIndex ;
-        int len = arr.length;
-        for (int i = 0; i < len; i++) {
-            maxIndex = 0;
-            for (int j = 0; j < len - i; j++) {
-                if (arr[j] > arr[maxIndex]){
-                    maxIndex = j;
-                }
-            }
-            int temp = arr[maxIndex];
-            arr[maxIndex] = arr[len - 1- i];
-            arr[len - 1 - i] = temp;
-        }
-        return arr;
+        return null;
     }
 }
